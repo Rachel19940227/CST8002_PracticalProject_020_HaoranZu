1f9549df56aca8e8afdc00f963e1b3ffaf7a37bb
Rachel19940227 <zu000001@algonquinlive.com>
2025-07-10 17:34:43 -0400

    modifying record-object (if needed) to support sorting

--- PresentationLayer/view.py
+++ PresentationLayer/view.py
@@ -46,6 +46,7 @@ class ConsoleView:
     def show_all(records):
         """
         Displays all DataRecord (or BaseRecord) objects from a list in a formatted table.
+        I change the sorting method that just show 4 coloums to make it easier to read.
 
         Args:
             records (list): A list of DataRecord or BaseRecord instances.
