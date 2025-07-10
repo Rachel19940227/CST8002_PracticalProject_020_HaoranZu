7f0283c3528725162ba620bcecf39b934bd31b2c
Rachel19940227 <zu000001@algonquinlive.com>
2025-07-10 17:33:05 -0400

    writing code to sort records

--- BusinessLayer/data_controller.py
+++ BusinessLayer/data_controller.py
@@ -1,10 +1,11 @@
 """
 Course: CST8002 Programming Language Research Project
-Professor: Stanley Pieda
-Due Date: 2025-06-15
+Professor: Tyler DeLa
+Due Date: 2025-07-13
 Author: Haoran Zu
-Description: Part of solution for Practical Project 2
+Description: Part of solution for Practical Project 3
 """
+
 
 
 
